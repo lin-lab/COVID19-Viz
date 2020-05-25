fa29f3d4377bb18c6ca83479ad0263c79134a12e
Andy Shi <andyshi@g.harvard.edu>
2020-05-24 23:35:36 -0400

    Clean Hopkins results. WIP: Need to merge some counties.
    Former-commit-id: d73f3badf64efcfd733ee09de3ef85e7087a3ce9

--- clean_data.R
+++ clean_data.R
@@ -1,12 +1,13 @@
 library(readr)
 library(dplyr)
 library(tidyr)
-library(sp)
-library(tigris)
 library(lubridate)
 library(stringr)
 library(magrittr)
 library(data.table)
+library(sf)
+library(tigris)
+library(USAboundaries)
 
 POS_CUTOFF <- 50
 START_DATE <- ymd("2020-03-19")
@@ -17,38 +18,29 @@ data(fips_codes)
 ########################################################################
 ## State-level data
 ########################################################################
-state_rt_wide <- read_csv("raw_data/State_Specific_Rt_5.5_8pm.csv") %>%
-  rename(State = Location) %>%
-  filter(State != "USA")
-rt_cols <- grepl("[[:digit:]]$", colnames(state_rt_wide))
-colnames(state_rt_wide)[rt_cols] <- paste0(colnames(state_rt_wide)[rt_cols], "_RtOrig")
 
-# make data into long format
-state_rt_long <- state_rt_wide %>%
-  pivot_longer(-c(FIPS, State),
-               names_to = c("Date", "variable"),
-               names_pattern = "([0-9\\-]+)_(.*)",
-               values_to = "value") %>%
-  pivot_wider(names_from = variable, values_from = value) %>%
-  mutate(Date = ymd(Date)) %>%
-  filter(Date >= START_DATE) %>%
-  rename(posIncr = pos_incr, totalPos = total_pos) %>%
+state_rt_long <- read_csv("raw_data/jhu_state_rt.csv") %>%
+  select(stateName, date, mean_rt, ci_lower, ci_upper, positive, death) %>%
   data.table()
-
 # when number of cases is too small, set Rt to NA
-state_rt_long[totalPos >= POS_CUTOFF, Rt := RtOrig]
-state_rt_long[totalPos < POS_CUTOFF, Rt := NA]
-# one day in Rhode Island has Rt of >100 probably because of reporting
-# Set those as Na also
-state_rt_long[RtOrig > 100, Rt := NA]
+state_rt_long[positive >= POS_CUTOFF,
+              `:=`(Rt_plot = mean_rt,
+                   Rt_upr = ci_upper,
+                   Rt_lwr = ci_lower)]
+state_rt_long[positive < POS_CUTOFF,
+              `:=`(Rt_plot = NA, Rt_upr = NA, Rt_lwr = NA)]
 
+# make wide format
 state_rt_tomerge <- state_rt_long %>%
-  pivot_wider(id_cols = c(FIPS, State), names_from = Date,
-              values_from = c(posIncr, totalPos, Rt))
+  select(stateName, date, Rt_plot, Rt_upr, Rt_lwr, positive, death) %>%
+  pivot_wider(id_cols = c(stateName), names_from = date,
+              values_from = c(Rt_plot, Rt_upr, Rt_lwr, positive, death))
 
-state_maps <- states(cb = TRUE, resolution = "20m")
-state_merged <- merge(state_maps, state_rt_tomerge, by.x = "NAME",
-                      by.y = "State", all.x = FALSE)
+state_maps <- us_states()
+state_merged <- merge(state_maps, state_rt_tomerge, by.x = "state_name",
+                      by.y = "stateName", all.x = FALSE) %>%
+  rename(stateName = state_name)
+
 saveRDS(state_merged, "clean_data/state_merged.rds")
 saveRDS(as_tibble(state_rt_long), "clean_data/state_rt_long.rds")
 
@@ -56,79 +48,70 @@ saveRDS(as_tibble(state_rt_long), "clean_data/state_rt_long.rds")
 ## County-level data
 ########################################################################
 
-county_maps <- counties(cb = TRUE, resolution = "20m")
-county_rt_wide <- read_csv("raw_data/County_Specific_Rt_5.5_8pm.csv") %>%
-  mutate(FIPS_str = sprintf("%05d", FIPS))
-rt_cols <- grepl("[[:digit:]]$", colnames(county_rt_wide))
-colnames(county_rt_wide)[rt_cols] <- paste0(colnames(county_rt_wide)[rt_cols], "_RtOrig")
+county_maps <- us_counties()
+county_rt_long <- read_csv("raw_data/jhu_county_rt.csv",
+                           guess_max = 10000) %>%
+  select(UID, date, county, stateName, FIPS, positive, death, mean_rt,
+         ci_lower, ci_upper) %>%
+  data.table()
+# when number of cases is too small, set Rt to NA
+county_rt_long[positive >= POS_CUTOFF,
+               `:=`(Rt_plot = mean_rt,
+                    Rt_upr = ci_upper,
+                    Rt_lwr = ci_lower)]
+county_rt_long[positive < POS_CUTOFF,
+               `:=`(Rt_plot = NA, Rt_upr = NA, Rt_lwr = NA)]
+county_rt_long[mean_rt >= 10 & positive >= POS_CUTOFF,]
+stopifnot(all(county_rt_long$Rt_upr >= county_rt_long$Rt_lwr, na.rm = TRUE))
 
-# take care of two counties whose names and FIPS have changed
+# TODO: Weird counties. Deal with this later
+weird_counties <- county_rt_long[is.na(FIPS),
+                                 .(county, stateName, UID)] %>%
+  distinct()
+weird_counties
+
+max_date <- max(county_rt_long$date)
+
+county_rt_long %>%
+  filter(stateName == "Massachusetts", date == max_date)
+county_rt_long %>%
+  filter(stateName == "New York", date == max_date)
+county_rt_long %>%
+  filter(stateName == "Missouri", date == max_date)
+county_rt_long %>%
+  filter(stateName == "Utah", date == max_date)
+
+county_rt_long %>%
+  filter(county == "Kansas City")
+
+county_rt_wide <- county_rt_long %>%
+  filter(!(UID %in% weird_counties$UID)) %>%
+  select(county, stateName, UID, FIPS, date, Rt_plot, Rt_upr, Rt_lwr,
+         positive, death) %>%
+  pivot_wider(id_cols = c(county, stateName, UID, FIPS),
+              names_from = date,
+              values_from = c(Rt_plot, Rt_upr, Rt_lwr, positive, death))
+
+# check if there will be counties that don't merge with the county maps
 state_fips <- fips_codes %>%
   select(state_code, state_name) %>%
   distinct()
-maps_df <- data.frame(GEOID = county_maps$GEOID,
-                      name = county_maps$NAME,
-                      statefp = county_maps$STATEFP,
+# data frame of FIPS in the map
+maps_df <- data.frame(geoid = county_maps$geoid,
+                      name = county_maps$name,
+                      statefp = county_maps$statefp,
                       stringsAsFactors = FALSE) %>%
   inner_join(state_fips, by = c("statefp" = "state_code"))
 
-new_names <- anti_join(maps_df, county_rt_wide,
-                       by = c("GEOID" = "FIPS_str")) %>%
-  filter(state_name != "Puerto Rico")
-new_names
 old_names <- anti_join(county_rt_wide, maps_df,
-                       by = c("FIPS_str" = "GEOID")) %>%
-  select(FIPS_str, County, State)
-old_names
-stopifnot(nrow(new_names) == nrow(old_names))
+                       by = c("FIPS" = "geoid")) %>%
+  select(FIPS, county, stateName)
+print(old_names, n = Inf)
 
-# replace the names
-for (i in 1:nrow(new_names)) {
-  index <- county_rt_wide$FIPS_str == old_names$FIPS_str[i]
-  county_rt_wide$FIPS_str[index] <- new_names$GEOID[i]
-  stopifnot(county_rt_wide$County[index] == old_names$County[i])
-  county_rt_wide$County[index] <- new_names$name[i]
-}
-
-# check that the replacement happened
-nrows_new <- county_rt_wide %>%
-  filter(FIPS_str %in% new_names$GEOID) %>%
-  nrow()
-stopifnot(nrows_new == nrow(new_names))
-nrows_old <- county_rt_wide %>%
-  filter(FIPS_str %in% old_names$FIPS_str) %>%
-  nrow()
-stopifnot(nrows_old == 0)
-
-# convert wide data to long data
-county_rt_long <- county_rt_wide %>%
-  select(-FIPS) %>%
-  pivot_longer(-c(FIPS_str, County, State),
-               names_to = c("Date", "variable"),
-               names_pattern = "([0-9\\-]+)_(.*)",
-               values_to = "value") %>%
-  pivot_wider(names_from = variable, values_from = value) %>%
-  mutate(Date = ymd(Date)) %>%
-  filter(Date >= START_DATE) %>%
-  rename(posIncr = pos_incr, totalPos = total_pos) %>%
-  data.table()
-# set Rt based on number of positive cases
-county_rt_long[totalPos >= POS_CUTOFF, Rt := RtOrig]
-county_rt_long[totalPos < POS_CUTOFF, Rt := NA]
-
-# one day in Rhode Island has Rt of >100 probably because of reporting
-# Set those as Na also
-county_rt_long[RtOrig > 100, Rt := NA]
-
-# prepare to merge
-county_rt_tomerge <- county_rt_long %>%
-  pivot_wider(id_cols = c(FIPS_str, County, State),
-              names_from = Date,
-              values_from = c(posIncr, totalPos, Rt))
 
 # do the merge
-county_merged <- merge(county_maps, county_rt_tomerge, by.x = "GEOID",
-                       by.y = "FIPS_str", all.x = FALSE)
+county_merged <- merge(county_maps, county_rt_wide, by.x = "geoid",
+                       by.y = "FIPS", all.x = FALSE)
 
 saveRDS(county_merged, "clean_data/county_merged.rds")
 saveRDS(as_tibble(county_rt_long), "clean_data/county_rt_long.rds")
@@ -137,13 +120,18 @@ saveRDS(as_tibble(county_rt_long), "clean_data/county_rt_long.rds")
 ## County and state names
 ########################################################################
 
-state_names1 <- state_rt_wide %>%
-  filter(State != "District Of Columbia") %>%
-  use_series(State) %>%
+state_names1 <- state_merged %>%
+  filter(stateName != "District of Columbia") %>%
+  use_series(stateName) %>%
   paste("State", sep = " ")
 
-state_names <- c(state_names1, "District Of Columbia")
-county_names <- paste(county_rt_tomerge$County, county_rt_tomerge$State,
+
+
+state_names <- c(state_names1, "District of Columbia")
+county_names <- paste(county_merged$county, county_merged$stateName,
                       sep = ", ")
 state_county_choices <- c(state_names, county_names)
 saveRDS(state_county_choices, "clean_data/state_county_choices.rds")
+
+state_choices <- unique(county_merged$stateName)
+saveRDS(state_choices, "clean_data/state_choices.rds")
