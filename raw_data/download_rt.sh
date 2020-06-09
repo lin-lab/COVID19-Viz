a9122915fb3f2a9e347b9bc71d614144307919cf
Andy Shi <andyshi@g.harvard.edu>
2020-06-09 14:21:46 -0400

    explicity state output file name
    Former-commit-id: 5cbdfae805f4fed8783a1e8a627bb24fff1e0517

--- raw_data/download_rt.sh
+++ raw_data/download_rt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_county_rt.tsv
-wget https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_state_rt.tsv
-wget https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_global_rt.tsv
+wget -O jhu_county_rt.tsv https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_county_rt.tsv
+wget -O jhu_state_rt.tsv https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_state_rt.tsv
+wget -O jhu_global_rt.tsv https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_global_rt.tsv
