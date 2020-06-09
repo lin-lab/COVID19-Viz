5fef406904e4e1390a9c143309ad922cfb1f8e87
Andy Shi <andyshi@g.harvard.edu>
2020-06-09 14:27:59 -0400

    get the raw data, not html page
    Former-commit-id: 409bc7c17b6c71ee389b83a7c5cfa4b7c8912bad

--- raw_data/download_rt.sh
+++ raw_data/download_rt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget -O jhu_county_rt.tsv https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_county_rt.tsv
-wget -O jhu_state_rt.tsv https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_state_rt.tsv
-wget -O jhu_global_rt.tsv https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_global_rt.tsv
+base_url=https://raw.githubusercontent.com/lin-lab/COVID19-Rt/master/initial_estimates/
+
+wget -O jhu_county_rt.tsv ${base_url}/jhu_county_rt.tsv
+wget -O jhu_state_rt.tsv ${base_url}/jhu_state_rt.tsv
+wget -O jhu_global_rt.tsv ${base_url}/jhu_global_rt.tsv
