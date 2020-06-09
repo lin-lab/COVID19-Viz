665118e5bdcd53799197622459b526c26558ba76
Andy Shi <andyshi@g.harvard.edu>
2020-06-08 22:11:13 -0400

    add script to download Rt data
    Former-commit-id: c2a7b37906f41ee8339453b474fd0b2a01cbabd3

---
+++ raw_data/download_rt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_county_rt.tsv
+wget https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_state_rt.tsv
+wget https://github.com/lin-lab/COVID19-Rt/blob/master/initial_estimates/jhu_global_rt.tsv
