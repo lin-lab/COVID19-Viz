8b3f4350295031d1a82d8ef0b1c0e1610df6d3ae
Andy Shi <andyshi@g.harvard.edu>
2020-09-10 10:34:45 -0400

    Call code to upload to gdrive
    Former-commit-id: d994b2d195114638e528e9461f2ff5873108f7a5
    Former-commit-id: 31541bdf2c3c2e956e2475ce5f10d7b6ce60026d

--- prep_data.sh
+++ prep_data.sh
@@ -5,3 +5,6 @@ Rscript --no-save --no-restore 02_clean_merge.R
 
 # zip data for github
 zip clean_data/rt_table_export.csv.zip clean_data/rt_table_export.csv
+
+# Upload unzipped data to Google
+Rscript --no-save --no-restore 03_upload_to_gdrive.R
