294f4081e9f7356fda094effa304fea95cddbb5a
Andy Shi <andyshi@g.harvard.edu>
2020-12-07 16:36:33 -0500

    use custom Rscript path

--- prep_data.sh
+++ prep_data.sh
@@ -2,6 +2,13 @@
 
 set -euxo pipefail
 
+# set Rscript
+RSCRIPT=/opt/R/4.0.3/bin/Rscript
+if [[ ! -f ${RSCRIPT} ]]
+then
+    RSCRIPT=/usr/bin/Rscript
+fi
+
 ./01_download_rt.sh
-Rscript --no-save --no-restore 02_clean_merge.R
+${RSCRIPT} --no-save --no-restore 02_clean_merge.R
 ./03_upload_to_aws.sh
