23c88168ca7e1b2016d08e08d9ff050b94ddd99e
Andy Shi <andyshi@g.harvard.edu>
2021-02-21 20:50:18 -0500

    add favicon

--- app.R
+++ app.R
@@ -766,6 +766,8 @@ ui <- function(req) {
     )
   ),
   dashboardBody(
+    tags$head(tags$link(rel = "shortcut icon", type = "image/png",
+                        href = "covid19logo2.png")),
     tags$head(tags$style(type="text/css", "div.info.legend.leaflet-control br {clear: both;}")),
     tags$head(tags$style(type = "text/css", "body {font-size: 16px} .aboutpage {font-size: 16px}")),
     #tags$head(tags$script(src = "shinyjs_funs.js")),
