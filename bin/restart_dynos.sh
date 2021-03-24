e94381975b05da7be6c311b0e5189776d0718272
Andy Shi <andyshi@g.harvard.edu>
2021-03-23 23:13:40 -0400

    modify restart dynos script

--- bin/restart_dynos.sh
+++ bin/restart_dynos.sh
@@ -2,5 +2,6 @@
 
 APP_ID_OR_NAME=hsph-covid-study
 curl -n -X DELETE https://api.heroku.com/apps/$APP_ID_OR_NAME/dynos \
+    -H "Authorization: Bearer ${HEROKU_API_KEY}" \
     -H "Content-Type: application/json" \
     -H "Accept: application/vnd.heroku+json; version=3"
