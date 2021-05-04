4338a9a87cf38f24f9c02bb1a6b4aac0f5100792
Andy Shi <andyshi@g.harvard.edu>
2021-05-04 13:57:31 -0400

    delete bin folder

--- bin/restart_dynos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-APP_ID_OR_NAME=hsph-covid-study
-curl -n -X DELETE https://api.heroku.com/apps/$APP_ID_OR_NAME/dynos \
-    -H "Authorization: Bearer ${HEROKU_API_KEY}" \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/vnd.heroku+json; version=3"
